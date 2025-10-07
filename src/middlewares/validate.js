471c13dc81724b9bb037a8dbebab9190a6a2f386
pablovalledebenitobrooktec <pablo.valledebenito@brooktec.com>
2025-10-07 14:06:50 +0200

    64148_pr_fixes

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,41 +1,31 @@
 const { StatusCodes } = require('http-status-codes');
 
 const validation = (schemas) => {
-
     return (req, res, next) => {
-            
-        try{
-            if(schemas.body){
-            const { error, value } = schemas.body.validate( req.body, {abortEarly: false});
-
-            if(error){
-                const err = new Error('Validation error');
-                err.status = StatusCodes.BAD_REQUEST;
-                err.details = error.details.map(d => d.message);
-
-                return next(err);
+        const errors = [];
+        ['body', 'params', 'target'].forEach(target => {
+            if(schemas[target]){
+                const {error, value} = schemas[target].validate(req[target], {abortEarly: false});
+                if(error){
+                    errors.push(...error.details.map(d => ({
+                        target,
+                        message: d.message,
+                        path: d.path.join('.')
+                    })));
+                } else {
+                    req[target] = value;
+                }
             }
-            req.body = value;
+        });
 
-        }
-        if(schemas.params){
-            const { error, value } = schemas.params.validate( req.params, {abortEarly: false});
-
-            if(error){
-                const err = new Error('Validation error');
-                err.status = StatusCodes.BAD_REQUEST;
-                err.details = error.details.map(d => d.message);
-
-                return next(err);
-            }
-            req.params = value;
-        }
-            return next();
-
-        }catch(err){
+        if(errors.length > 0){
+            const err = new Error('Validation error');
+            err.status = 400;
+            err.details = errors;
             return next(err);
         }
-    };
-};
+        next();
+    }
+}
 
 module.exports = validation;
